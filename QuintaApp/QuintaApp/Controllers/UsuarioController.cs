8814cb1d4e7e573b535ec1a4c479a77da1d6f29d
Mikhail Bispo <anaomsb@hotmail.com>
2020-04-30 00:10:45 -0300

    Inclusoes de Model, Controller e View
    Foi incluido o controller, View e Model do usuario.

---
+++ QuintaApp/QuintaApp/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuintaApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuintaApp/QuintaApp/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuintaApp.Models
+{
+    public class Usuario
+    {
+        public short UsuID { get; set; }
+        public string Nome { get; set; }
+        public string Obs { get; set; }
+        public DateTime Nasc { get; set; }
+        public string Email { get; set; }
+        public string Lgoin { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+    }
+}
